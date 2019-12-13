699dc9de272e3391a8617a48e9e0225035f2de02
jack <1258040085@qq.com>
2019-12-13 15:55:46 +0800

    保存代码

--- lfb_backend/account/lfb_account/apis/views.py
+++ lfb_backend/account/lfb_account/apis/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 
 from core.mixins.response import ResponseMixin
+from core.utils import Validators
 
 from ..models import User
 
@@ -8,7 +9,26 @@ from ..models import User
 class UserDetail(ResponseMixin, View):
 
     def post(self, request):
+
         username = request.user.username
         user = User.object_list.get_by_field(username=username)
 
         return self.get_json_response(user)
+
+
+class UserCreation(ResponseMixin, View):
+
+
+
+    def post(self, request):
+
+        args = request.POST.copy()
+
+        if args:
+
+            validator = Validators(request_body=args)
+            username = validator.args_validator(
+                arg_key='username',
+                null=False,
+                valid_type=str)
+
